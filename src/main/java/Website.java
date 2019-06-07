380a57447997098e0c921e0da59f53acacd0446f
Shinichi17CP <quynh.pham17@imperial.ac.uk>
2019-06-07 23:55:42 +0100

    Changed parameter for favourite

--- src/main/java/Website.java
+++ src/main/java/Website.java
@@ -24,7 +24,7 @@ public class Website extends HttpServlet {
             JSONArray recipes =
                 fetchMatchingRecipes(new JSONArray(req.getParameter("owned")));
             writer.print(recipes);
-        } else if (param != null && param.equals("recipe")) {
+        } else if (param != null && param.equals("favourite")) {
             Recipe recipe = Database.getRecipeInformation(
                 Database.getRecipeIdFromName(req.getParameter("name")), 0);
             writer.print(encodeRecipe(recipe));
